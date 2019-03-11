df6433c2e60e5197a7a3e6523e4bfedf2539f971
mark-marshall <mark.marshallgp@gmail.com>
2019-03-11 09:01:22 +0000

    Added input handling with state management for Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    user: {
+      email: '',
+      password: '',
+    },
+  };
+
+  userValuesSet = event => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
         <h3>Login</h3>
         <div>
-          Email: <input />
+          Email:{' '}
+          <input
+            name="email"
+            value={this.state.user.email}
+            onChange={this.userValuesSet}
+          />
         </div>
         <div>
-          Password: <input />
+          Password:{' '}
+          <input
+            name="password"
+            value={this.state.user.password}
+            onChange={this.userValuesSet}
+          />
         </div>
         <button>Log In</button>
       </div>
