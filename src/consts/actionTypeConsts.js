5fd76f3e0dcb8af95d87d33ab0686f33e4a4bd05
mark-marshall <mark.marshallgp@gmail.com>
2019-03-13 13:01:51 +0000

    Added comments on consts files

--- src/consts/actionTypeConsts.js
+++ src/consts/actionTypeConsts.js
@@ -1,31 +1,30 @@
+// Token handling consts
 export const FETCHING_TOKEN = "FETCHING_TOKEN";
 export const TOKEN_FETCHED = "TOKEN_FETCHED";
 export const ERROR_FETCHING_TOKEN = "ERROR_FETCHING_TOKEN";
-
+// User fecthing consts
 export const FETCHING_USERS = "FETCHING_USERS";
 export const USERS_FETCHED = "USERS_FETCHED";
 export const PUSH_USERS = "PUSH_USERS";
 export const ERROR_FETCHING_USERS = "ERROR_FETCHING_USERS";
-
+// CRUD error consts
 export const ERROR_ADDING_ITEM = "ERROR_ADDING_ITEM";
-
 export const ERROR_DELETING_ITEM = "ERROR_DELETING_ITEM";
-
 export const ERROR_UPDATING_ITEM = "ERROR_UPDATING_ITEM";
-
+// Filter and search consts
 export const FILTER_ITEMS = "FILTER_ITEMS";
 export const SEARCH_ITEMS = "SEARCH_ITEMS";
-
+// Item fetching consts
 export const FETCHING_ITEMS = "FETCHING_ITEMS";
 export const ITEMS_FETCHED = "ITEMS_FETCHED";
 export const PUSH_ITEMS = "PUSH_ITEMS";
 export const ERROR_FETCHING_ITEMS = "ERROR_FETCHING_ITEMS";
-
+// User fetching consts
 export const FETCHING_USER = "FETCHING_USER";
 export const USER_FETCHED = "USER_FETCHED";
 export const PUSH_USER = "PUSH_USER";
 export const ERROR_FETCHING_USER = "ERROR_FETCHING_USER";
-
+// Login consts
 export const SET_VOLUNTEER_LOGIN = "SET_VOLUNTEER_LOGIN";
-
+// Logout consts
 export const LOGOUT = "LOGOUT";
--- src/consts/apiConsts.js
+++ src/consts/apiConsts.js
@@ -1,3 +1,4 @@
+// API consts
 export const staffRegisterURL = 'https://soup-kitchen-backend.herokuapp.com/api/staff/register';
 export const staffLoginURL = 'https://soup-kitchen-backend.herokuapp.com/api/staff/login';
 export const getItemsURL = 'https://soup-kitchen-backend.herokuapp.com/api/items';
