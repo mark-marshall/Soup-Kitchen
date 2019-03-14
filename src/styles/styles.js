4509a872931ba2d5acc1f41ebd3f2b88a3c3f964
mark-marshall <mark.marshallgp@gmail.com>
2019-03-14 08:48:15 +0000

    Added global CSS resets

---
+++ src/styles/styles.js
@@ -0,0 +1,12 @@
+const soupStyles = {
+    color: {
+        primary: '#F2AF9A',
+        secondary: '#CF2C51',
+        accent: '#3D075E',
+        success: '#2F6C67',
+        danger: '#EB4E47;',
+        neutral: '#f6f6f6',
+    }
+}
+
+export default soupStyles;
